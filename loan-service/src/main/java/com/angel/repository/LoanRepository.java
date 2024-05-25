883c2f6dc875942696603d3dbc2c50e09547970b
r_angel <143842574+6angel6@users.noreply.github.com>
2024-05-25 22:40:39 +0500

    -added Loan Repository

---
+++ loan-service/src/main/java/com/angel/repository/LoanRepository.java
@@ -0,0 +1,9 @@
+package com.angel.repository;
+
+import com.angel.model.LoanEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoanRepository extends JpaRepository<LoanEntity, Long> {
+}
