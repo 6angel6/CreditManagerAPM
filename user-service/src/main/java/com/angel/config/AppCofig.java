8edcf886dfdd545e16ff90cdd90dad1b53380d60
r_angel <143842574+6angel6@users.noreply.github.com>
2024-06-02 01:15:16 +0500

    -added restTemplate bean

--- user-service/src/main/java/com/angel/config/AppCofig.java
+++ user-service/src/main/java/com/angel/config/AppCofig.java
@@ -3,8 +3,10 @@ package com.angel.config;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.Clock;
 import java.time.format.DateTimeFormatter;
@@ -27,5 +29,11 @@ public class AppCofig {
 	@Bean
 	public Clock clock() {
 		return Clock.systemUTC();
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 }
